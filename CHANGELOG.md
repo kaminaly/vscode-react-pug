9db23ee8916e6c5d1c65a65e2f0d94c884c7c330
Hiroaki Yasutomo <yasutomo@shiftbrain.co.jp>
2018-09-07 21:35:43 +0900

    bugfix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Release Notes
 
+## 0.0.2
+
+bugfix: end of regular expression is changed from `` ` `` to ``\\s*(`)``.
+bugfix: comment shortcut is changed from `//` to `//-`. 
+
 ## 0.0.1
 
 Initial release of vscode-react-pug
--- test.js
+++ test.js
@@ -11,3 +11,16 @@ export default (props) => pug`
       for num, i in [1,2,3]
         li(key=i)= num
 `
+
+export default (props) => {
+  return pug`
+    //- some comment 
+    .Component
+      if props.showSubComponent
+        SubComponent.someClass(...props)
+
+      ul
+        for num, i in [1,2,3]
+          li(key=i)= num
+  `
+}
