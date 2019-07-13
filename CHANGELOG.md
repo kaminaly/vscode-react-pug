c006af9ead1a61513b130b74c64b5f9bf86a4690
Hiroaki Yasutomo <yasutomo@shiftbrain.co.jp>
2019-07-14 00:56:33 +0900

    bugfix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,15 @@
 # Release Notes
 
+## 0.0.5
+
+bugfix: start of regular expression is changed from `` pug` `` to `` pug\\s*` ``.
+bugfix: end of regular expression is changed from `` ` `` to `` \\s*` ``.
+test.js (sample code) is updated.
+
+## 0.0.4
+
+Make code shorter
+
 ## 0.0.2
 
 bugfix: end of regular expression is changed from `` ` `` to ``\\s*(`)``.
--- test.js
+++ test.js
@@ -10,10 +10,13 @@ export default (props) => pug`
     ul
       for num, i in [1,2,3]
         li(key=i)= num
-`
+` // Here will be JS commnet
+// Here will be JS commnet
 
 export default (props) => {
+  // Here will be JS commnet
   return pug`
+    // Here will be HTML comment
     //- some comment 
     .Component
       if props.showSubComponent
@@ -22,5 +25,10 @@ export default (props) => {
       ul
         for num, i in [1,2,3]
           li(key=i)= num
-  `
+      
+  ` // Here will be JS commnet
+  // Here will be JS commnet
 }
+
+// It doesn't work...
+export default (props) => pug`.Component inline test`
